60a332373987a7b91a63983dab36514e96dbf98a
hengamehakbari <h.akbari7778@gmail.com>
2025-06-14 16:30:30 +0330

    add jsdoc

--- Src/Application/order.js
+++ Src/Application/order.js
@@ -1,7 +1,21 @@
+/**
+ * @memberOf OrderService.Src.Application.order
+ * @summary Deduct a amount from inventory
+ * @description Get a value and deduct it from inventory
+ * @param {Number} amount amount of asset
+ * @return {Promise<Object>} Promise {output, state}
+ */
 export function deduct(amount) {
     return amount
 }
 
+/**
+ * @memberOf OrderService.Src.Application.order
+ * @summary check if inventory has more than amount
+ * @description Return true if we have enough in inventory, else returns false
+ * @param {Number} amount amount of asset
+ * @return {Promise<Boolean>} Promise
+ */
 export function inquiry(amount) {
     return amount > 10
 }
