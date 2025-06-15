fd9ed134e2e77a0dd622edf24a2f4860f10e8196
hengamehakbari <h.akbari7778@gmail.com>
2025-06-15 22:38:03 +0330

    hot fix

--- Src/Controller/orderController.js
+++ Src/Controller/orderController.js
@@ -14,15 +14,17 @@ export class OrderController {
      * @return {Promise<Boolean>} Promise return true if order submitted, return false if order is invalid
      */
     async makeOrder(amount) {
-        // const lockObj = await this.lock.lock();
+
         let canBuy;
         try {
+            // const lockObj = await this.lock.lock();
             canBuy = await Order.canBuy(amount, this.cache);
             let remain = -1;
-            if (canBuy)
+            if (canBuy) {
                 remain = await Order.deduct(amount, this.cache);
+                await await Order.addToQueue({ currentInventory: amount + remain, bought: amount }, this.eventQueue);
+            }
             // await this.lock.unlock(lockObj);
-            await await Order.addToQueue({ currentInventory: amount + remain, bought: amount }, this.eventQueue);
         } catch (error) {
             // Rollback if could not send
             await Order.deductRollback(amount, this.cache);
